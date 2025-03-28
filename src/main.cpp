b4b00fa9ad5b5dd0c8e6689447504b304b6d4ade
enfmarinho <eduardonestormarinho228@gmail.com>
2025-03-28 20:36:39 -0300

    Fixing some stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,8 @@
 #include "parser.tab.hpp"
-#include <iostream>
 
 extern int yyparse();
 
 int main(int argc, char *argv[]) {
-  std::cout << "parsing... " << std::endl;
   yyparse();
-  std::cout << "done" << std::endl;
   return 0;
 }
