08fb733f5dca344dbadca10fa5fa41edfd3f98e0
enfmarinho <eduardonestormarinho228@gmail.com>
2025-04-26 00:05:23 -0300

    Implementing recursive LL algorithm

--- extras/ll1.cpp
+++ extras/ll1.cpp
@@ -1,4 +1,5 @@
 #include <cassert>
+#include <cstring>
 #include <iostream>
 #include <stack>
 #include <vector>
@@ -10,25 +11,50 @@
 #define SymbolType int
 
 // Global variables
-YYSTYPE yylval;
+YYSTYPE yylval; // Used be Flex
 std::vector<SymbolType> table[NUMBER_OF_NON_TERMINALS][NUMBER_OF_TERMINALS];
 
+// 1d for the symbols, 2d for each rule of the symbol, 3d for each symbol of the
+// rule
+std::vector<std::vector<SymbolType>>
+    rules[NUMBER_OF_TERMINALS + NUMBER_OF_NON_TERMINALS];
+
 // Functions signatures
-bool LL(std::stack<SymbolType> &stack);
+bool LL_table(std::stack<SymbolType> &stack);
 std::vector<SymbolType> &consult_table(SymbolType non_terminal,
                                        SymbolType terminal);
-bool is_terminal(SymbolType symbol);
-void error();
 void init_table();
 
-int main(int argc, char *argv[]) {
-  init_table();
+bool LL_rec(std::stack<SymbolType> &stack);
+std::vector<std::vector<SymbolType>> &consult_rules(SymbolType symbol);
+void init_rules();
 
+bool is_terminal(SymbolType symbol);
+void error();
+void print_usage();
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    print_usage();
+    return 1;
+  }
+
+  // Initializing LL stack
   std::stack<SymbolType> stack;
   stack.push(END);
-  stack.push(PROGRAM);
+  stack.push(PROGRAM); // Start symbol
 
-  bool reached_input_end = LL(stack);
+  bool reached_input_end;
+  if (std::strcmp(argv[1], "-table") == 0) {
+    init_table();
+    reached_input_end = LL_table(stack);
+  } else if (std::strcmp(argv[1], "-rec") == 0) {
+    init_rules();
+    reached_input_end = LL_rec(stack);
+  } else {
+    print_usage();
+    return 1;
+  }
 
   if (stack.empty() && reached_input_end) {
     std::cout << "Input belongs to grammar" << std::endl;
@@ -39,7 +65,7 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-bool LL(std::stack<SymbolType> &stack) {
+bool LL_table(std::stack<SymbolType> &stack) {
   SymbolType input_symbol;
   bool symbol_read = false;
   bool reached_input_end = false;
@@ -92,6 +118,63 @@ std::vector<SymbolType> &consult_table(SymbolType non_terminal,
   return table[non_terminal][terminal];
 }
 
+void init_table() {
+  // TODO
+}
+
+bool LL_rec(std::stack<SymbolType> &stack, SymbolType &curr_symbol,
+            bool &reached_input_end) {
+
+  if (reached_input_end) {
+    return false;
+  }
+
+  if (curr_symbol == 0 && !reached_input_end) {
+    curr_symbol = yylex();
+    if (curr_symbol == 0) {
+      curr_symbol = END;
+      reached_input_end = true;
+    }
+  }
+  SymbolType top_stack_symbol = stack.top();
+  if (is_terminal(top_stack_symbol)) {
+    if (top_stack_symbol == curr_symbol) {
+      curr_symbol = 0;
+      stack.pop();
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  for (const std::vector<SymbolType> &rule : consult_rules(top_stack_symbol)) {
+    // Try each of those rules hopping for a match
+    std::stack<SymbolType> stack_cp = stack;
+    stack.pop();
+    for (SymbolType s : rule) {
+      stack.push(s);
+    }
+    if (LL_rec(stack_cp, curr_symbol, reached_input_end)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+std::vector<std::vector<SymbolType>> &consult_rules(SymbolType symbol) {
+  if (symbol >= EMPTY) {
+    symbol = symbol - EMPTY + NUMBER_OF_NON_TERMINALS;
+    assert(symbol >= NUMBER_OF_NON_TERMINALS &&
+           symbol < NUMBER_OF_NON_TERMINALS + NUMBER_OF_TERMINALS);
+  }
+  return rules[symbol];
+}
+
+void init_rules() {
+  // TODO
+}
+
 bool is_terminal(SymbolType symbol) { return symbol >= EMPTY; }
 
 void error(SymbolType symbol) {
@@ -116,6 +199,8 @@ void error(SymbolType symbol) {
             << std::endl;
 }
 
-void init_table() {
-  // TODO
+void print_usage() {
+  std::cout << "Missing or invalid commend line argument! Must be -table or "
+               "-rec, representing the LL algorithm to be used"
+            << std::endl;
 }
