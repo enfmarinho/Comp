aa0441ee2eb899cfc3436e7b2967c1e65d8cbd3d
enfmarinho <eduardonestormarinho228@gmail.com>
2025-04-26 00:48:25 -0300

    Fixing LL algorithms

--- extras/ll1.cpp
+++ extras/ll1.cpp
@@ -1,6 +1,7 @@
 #include <cassert>
 #include <cstring>
 #include <iostream>
+#include <queue>
 #include <stack>
 #include <vector>
 
@@ -9,6 +10,7 @@
 #include "non_terminals.h"
 
 #define SymbolType int
+#define SymbolRecInputType std::pair<SymbolType, YYSTYPE>
 
 // Global variables
 YYSTYPE yylval; // Used be Flex
@@ -25,12 +27,15 @@ std::vector<SymbolType> &consult_table(SymbolType non_terminal,
                                        SymbolType terminal);
 void init_table();
 
-bool LL_rec(std::stack<SymbolType> &stack);
+bool LL_rec(std::stack<SymbolType> stack, std::queue<SymbolRecInputType> input);
 std::vector<std::vector<SymbolType>> &consult_rules(SymbolType symbol);
 void init_rules();
+// Since LL_rec performs backtracking, the input must be read and saved at the
+// beginning
+std::queue<SymbolRecInputType> read_rec_input();
 
 bool is_terminal(SymbolType symbol);
-void error();
+void error(SymbolType symbol, const YYSTYPE &input_yylval = yylval);
 void print_usage();
 
 int main(int argc, char *argv[]) {
@@ -44,19 +49,20 @@ int main(int argc, char *argv[]) {
   stack.push(END);
   stack.push(PROGRAM); // Start symbol
 
-  bool reached_input_end;
+  bool sucessful_parse;
   if (std::strcmp(argv[1], "-table") == 0) {
     init_table();
-    reached_input_end = LL_table(stack);
+    sucessful_parse = LL_table(stack);
   } else if (std::strcmp(argv[1], "-rec") == 0) {
     init_rules();
-    reached_input_end = LL_rec(stack);
+    std::queue<SymbolRecInputType> input_symbols = read_rec_input();
+    sucessful_parse = LL_rec(stack, input_symbols);
   } else {
     print_usage();
     return 1;
   }
 
-  if (stack.empty() && reached_input_end) {
+  if (sucessful_parse) {
     std::cout << "Input belongs to grammar" << std::endl;
   } else {
     std::cout << "Input doesn't belong to grammar" << std::endl;
@@ -66,15 +72,15 @@ int main(int argc, char *argv[]) {
 }
 
 bool LL_table(std::stack<SymbolType> &stack) {
-  SymbolType input_symbol;
-  bool symbol_read = false;
+  SymbolType curr_input_symbol;
+  bool must_read_input_symbol = true;
   bool reached_input_end = false;
 
   while (!stack.empty() && !reached_input_end) {
     SymbolType top_stack_symbol = stack.top();
-    if (!symbol_read) {
+    if (must_read_input_symbol) {
       SymbolType symbol = yylex();
-      symbol_read = true;
+      must_read_input_symbol = false;
       if (symbol == 0) {
         symbol = END;
         reached_input_end = true;
@@ -82,38 +88,38 @@ bool LL_table(std::stack<SymbolType> &stack) {
     }
 
     if (is_terminal(top_stack_symbol)) {
-      if (top_stack_symbol == input_symbol) {
+      if (top_stack_symbol == curr_input_symbol) {
         stack.pop();
-        symbol_read = false;
+        must_read_input_symbol = true;
       } else {
-        error();
+        error(curr_input_symbol);
         break;
       }
-    } else if (!consult_table(top_stack_symbol, input_symbol)
+    } else if (!consult_table(top_stack_symbol, curr_input_symbol)
                     .empty()) { // If there is a relevant rule associated to the
                                 // non-terminal symbol at the top of the stack
       stack.pop();
-      for (SymbolType t : consult_table(top_stack_symbol, input_symbol)) {
+      for (SymbolType t : consult_table(top_stack_symbol, curr_input_symbol)) {
         if (t != EMPTY) { // Add symbols in table to the stack, except when
                           // symbol is empty
           stack.push(t);
         }
       }
     } else {
-      error();
+      error(curr_input_symbol);
       break;
     }
   }
 
-  return reached_input_end;
+  return reached_input_end && stack.empty();
 }
 
 std::vector<SymbolType> &consult_table(SymbolType non_terminal,
                                        SymbolType terminal) {
   terminal = terminal - EMPTY;
-  assert(non_terminal >= 0 && non_terminal <= NUMBER_OF_NON_TERMINALS &&
+  assert(non_terminal >= 0 && non_terminal < NUMBER_OF_NON_TERMINALS &&
          "Try to index Table with an out of bound index");
-  assert(terminal >= 0 && terminal <= NUMBER_OF_TERMINALS &&
+  assert(terminal >= 0 && terminal < NUMBER_OF_TERMINALS &&
          "Try to index Table with an out of bound index");
   return table[non_terminal][terminal];
 }
@@ -122,26 +128,21 @@ void init_table() {
   // TODO
 }
 
-bool LL_rec(std::stack<SymbolType> &stack, SymbolType &curr_symbol,
-            bool &reached_input_end) {
+bool LL_rec(std::stack<SymbolType> stack,
+            std::queue<SymbolRecInputType> input_symbols) {
 
-  if (reached_input_end) {
-    return false;
+  if (stack.empty() || input_symbols.empty()) {
+    return stack.empty() && input_symbols.empty();
   }
 
-  if (curr_symbol == 0 && !reached_input_end) {
-    curr_symbol = yylex();
-    if (curr_symbol == 0) {
-      curr_symbol = END;
-      reached_input_end = true;
-    }
-  }
+  auto [curr_input_symbol, curr_yylval] = input_symbols.front();
   SymbolType top_stack_symbol = stack.top();
+
   if (is_terminal(top_stack_symbol)) {
-    if (top_stack_symbol == curr_symbol) {
-      curr_symbol = 0;
+    if (top_stack_symbol == curr_input_symbol) {
       stack.pop();
-      return true;
+      input_symbols.pop();
+      return LL_rec(stack, input_symbols);
     } else {
       return false;
     }
@@ -150,23 +151,25 @@ bool LL_rec(std::stack<SymbolType> &stack, SymbolType &curr_symbol,
   for (const std::vector<SymbolType> &rule : consult_rules(top_stack_symbol)) {
     // Try each of those rules hopping for a match
     std::stack<SymbolType> stack_cp = stack;
-    stack.pop();
+    stack_cp.pop();
     for (SymbolType s : rule) {
-      stack.push(s);
+      stack_cp.push(s);
     }
-    if (LL_rec(stack_cp, curr_symbol, reached_input_end)) {
+    if (LL_rec(stack_cp, input_symbols)) {
       return true;
     }
   }
-
+  error(curr_input_symbol, curr_yylval);
   return false;
 }
 
 std::vector<std::vector<SymbolType>> &consult_rules(SymbolType symbol) {
-  if (symbol >= EMPTY) {
+  if (is_terminal(symbol)) {
     symbol = symbol - EMPTY + NUMBER_OF_NON_TERMINALS;
     assert(symbol >= NUMBER_OF_NON_TERMINALS &&
            symbol < NUMBER_OF_NON_TERMINALS + NUMBER_OF_TERMINALS);
+  } else {
+    assert(symbol >= 0 && symbol < NUMBER_OF_NON_TERMINALS);
   }
   return rules[symbol];
 }
@@ -175,32 +178,41 @@ void init_rules() {
   // TODO
 }
 
+std::queue<SymbolRecInputType> read_rec_input() {
+  std::queue<SymbolRecInputType> input_symbols;
+  SymbolType symbol;
+  while ((symbol = yylex()) != 0) {
+    input_symbols.push({symbol, yylval});
+  }
+  return input_symbols;
+}
+
 bool is_terminal(SymbolType symbol) { return symbol >= EMPTY; }
 
-void error(SymbolType symbol) {
+void error(SymbolType symbol, const YYSTYPE &input_yylval) {
   int line, column;
   if (symbol == INT_LITERAL) {
-    line = yylval.int_value.line;
-    column = yylval.int_value.column;
+    line = input_yylval.int_value.line;
+    column = input_yylval.int_value.column;
   } else if (symbol == FLOAT_LITERAL) {
-    line = yylval.float_value.line;
-    column = yylval.float_value.column;
+    line = input_yylval.float_value.line;
+    column = input_yylval.float_value.column;
   } else if (symbol == STRING_LITERAL) {
-    line = yylval.string_value.line;
-    column = yylval.string_value.column;
+    line = input_yylval.string_value.line;
+    column = input_yylval.string_value.column;
   } else if (symbol == ID) {
-    line = yylval.id.line;
-    column = yylval.id.column;
+    line = input_yylval.id.line;
+    column = input_yylval.id.column;
   } else {
-    line = yylval.general_values.line;
-    column = yylval.general_values.column;
+    line = input_yylval.general_values.line;
+    column = input_yylval.general_values.column;
   }
   std::cout << "Error at line " << line << "and at column " << column
             << std::endl;
 }
 
 void print_usage() {
-  std::cout << "Missing or invalid commend line argument! Must be -table or "
+  std::cout << "Missing or invalid command line argument! Must be -table or "
                "-rec, representing the LL algorithm to be used"
             << std::endl;
 }
