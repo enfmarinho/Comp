b3ffba5a37a9c14c7c81850d53dd707fa37d8d87
enfmarinho <eduardonestormarinho228@gmail.com>
2025-06-18 10:12:02 -0300

    Implementing Type constructors

---
+++ src/type.cpp
@@ -0,0 +1,21 @@
+#include "type.hpp"
+#include "../build/parser.tab.hpp"
+#include "symbol_table.hpp"
+
+Type::Type(yytokentype base_type) : base_type(base_type) {}
+
+Type::Type(yytokentype base_type, std::shared_ptr<Type> referenced_type)
+    : base_type(base_type), referenced_type(referenced_type) {}
+
+Type::Type(yytokentype base_type,
+           std::vector<std::shared_ptr<Type>> parem_types,
+           std::shared_ptr<Type> return_type)
+    : base_type(base_type), param_types(parem_types),
+      return_type(return_type){};
+
+Type::Type(yytokentype base_type, std::string struct_type_name,
+           SymbolTable *symbol_table_cp)
+    : base_type(base_type), struct_type_name(struct_type_name) {
+  symbol_table = new SymbolTable();
+  *symbol_table = *symbol_table_cp;
+}
--- src/type.hpp
+++ src/type.hpp
@@ -5,19 +5,8 @@
 #include <memory>
 #include <string>
 #include <vector>
-class SymbolTable;
 
-// enum BaseType {
-//   VOID,
-//   INT,
-//   FLOAT,
-//   STRING,
-//   BOOL,
-//   PROCEDURE,
-//   STRUCT,
-//   REFERENCE,
-//   ERROR,
-// };
+class SymbolTable;
 
 struct Type {
   yytokentype base_type;
@@ -26,8 +15,18 @@ struct Type {
   std::vector<std::shared_ptr<Type>>
       param_types;                     // For procedure type, empty otherwise
   std::shared_ptr<Type> return_type;   // For procedure type, nullptr otherwise
-  std::string struct_name;             // For struct types, empty otherwise
+  std::string struct_type_name;        // For struct types, empty otherwise
   SymbolTable *symbol_table = nullptr; // For struct types
+
+  Type(yytokentype base_type);
+
+  Type(yytokentype base_type, std::shared_ptr<Type> referenced_type);
+
+  Type(yytokentype base_type, std::vector<std::shared_ptr<Type>> parem_types,
+       std::shared_ptr<Type> return_type);
+
+  Type(yytokentype base_type, std::string struct_type_name,
+       SymbolTable *symbol_table_cp);
 };
 
 #endif // #ifndef TYPE_H
