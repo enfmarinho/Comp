e74c2225f6460b862493e5567fd6d2f5324587d7
enfmarinho <eduardonestormarinho228@gmail.com>
2025-06-20 17:52:04 -0300

    Adding BaseType enum

---
+++ src/base_type.hpp
@@ -0,0 +1,16 @@
+#ifndef BASE_TYPE_HPP
+#define BASE_TYPE_HPP
+
+enum BaseType {
+  Void,
+  Int,
+  Float,
+  Bool,
+  String,
+  Struct,
+  Procedure,
+  Ref,
+  Error,
+};
+
+#endif // !BASE_TYPE_HPP
