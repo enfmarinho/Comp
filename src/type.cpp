3b36f24d8baf4382041aa89123a4d08a2885d17f
enfmarinho <eduardonestormarinho228@gmail.com>
2025-06-21 16:24:01 -0300

    Removing usage of shared_ptr

--- src/type.cpp
+++ src/type.cpp
@@ -4,11 +4,11 @@
 
 Type::Type(BaseType base_type) : base_type(base_type) {}
 
-Type::Type(BaseType base_type, std::shared_ptr<Type> referenced_type)
+Type::Type(BaseType base_type, Type *referenced_type)
     : base_type(base_type), referenced_type(referenced_type) {}
 
-Type::Type(BaseType base_type, std::vector<std::shared_ptr<Type>> parem_types,
-           std::shared_ptr<Type> return_type)
+Type::Type(BaseType base_type, std::vector<Type *> parem_types,
+           Type *return_type)
     : base_type(base_type), param_types(parem_types),
       return_type(return_type) {};
 
--- src/type.hpp
+++ src/type.hpp
@@ -2,7 +2,6 @@
 #define TYPE_HPP
 
 #include "base_type.hpp"
-#include <memory>
 #include <string>
 #include <vector>
 
@@ -10,20 +9,17 @@ class SymbolTable;
 
 struct Type {
   BaseType base_type;
-  std::shared_ptr<Type>
-      referenced_type; // For Reference type, nullptr otherwise
-  std::vector<std::shared_ptr<Type>>
-      param_types;                     // For procedure type, empty otherwise
-  std::shared_ptr<Type> return_type;   // For procedure type, nullptr otherwise
-  std::string struct_type_name;        // For struct variables
-  SymbolTable *symbol_table = nullptr; // For struct declarations
+  Type *referenced_type{nullptr};     // For Reference type, nullptr otherwise
+  std::vector<Type *> param_types;    // For procedure type, empty otherwise
+  Type *return_type{nullptr};         // For procedure type, nullptr otherwise
+  std::string struct_type_name;       // For struct variables
+  SymbolTable *symbol_table{nullptr}; // For struct declarations
 
   Type(BaseType base_type);
 
-  Type(BaseType base_type, std::shared_ptr<Type> referenced_type);
+  Type(BaseType base_type, Type *referenced_type);
 
-  Type(BaseType base_type, std::vector<std::shared_ptr<Type>> parem_types,
-       std::shared_ptr<Type> return_type);
+  Type(BaseType base_type, std::vector<Type *> parem_types, Type *return_type);
 
   Type(BaseType base_type, std::string struct_type_name);
 
