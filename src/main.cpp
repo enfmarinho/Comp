e436f8ea3ffc36231ca77aea7e06c0f48b960737
enfmarinho <eduardonestormarinho228@gmail.com>
2025-07-02 14:57:34 -0300

    Opening output file

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include "parser.tab.hpp"
+#include <ifstream>
 
 extern int yyparse();
 
+std::ifstream file_out;
+
 int main(int argc, char *argv[]) {
+  if (argc > 1)
+    file_out = std::ifstream(argv[1]);
+  else
+    file_out = std::ifstream("three_address_code.c");
+
   yyparse();
+  file_out.close();
   return 0;
 }
